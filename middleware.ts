5757ce58b6c06337da41bb4d8a136106ad4986ae
Thura69 <spyking64@gmail.com>
2024-11-15 10:07:03 +0630

    chore(middleware)

--- middleware.ts
+++ middleware.ts
@@ -1,34 +1,29 @@
 import { NextRequest, NextResponse } from "next/server";
 
-const allowedOrigins = ["*"];
-
 const corsOptions = {
   "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
   "Access-Control-Allow-Headers": "Content-Type, Authorization",
 };
 
 export function middleware(request: NextRequest) {
-  // Check the origin from the request
-  const origin = request.headers.get("origin") ?? "";
-  const isAllowedOrigin = allowedOrigins.includes(origin);
+  const origin = request.headers.get("origin") || "";
 
-  // Handle preflighted requests
-  const isPreflight = request.method === "OPTIONS";
-
-  if (isPreflight) {
-    const preflightHeaders = {
-      ...(isAllowedOrigin && { "Access-Control-Allow-Origin": origin }),
-      ...corsOptions,
-    };
-    return NextResponse.json({}, { headers: preflightHeaders });
+  // Handle preflight requests
+  if (request.method === "OPTIONS") {
+    return NextResponse.json(
+      {},
+      {
+        headers: {
+          "Access-Control-Allow-Origin": origin,
+          ...corsOptions,
+        },
+      }
+    );
   }
 
-  // Handle simple requests
+  // Handle actual requests
   const response = NextResponse.next();
-
-  if (isAllowedOrigin) {
-    response.headers.set("Access-Control-Allow-Origin", origin);
-  }
+  response.headers.set("Access-Control-Allow-Origin", origin);
 
   Object.entries(corsOptions).forEach(([key, value]) => {
     response.headers.set(key, value);
